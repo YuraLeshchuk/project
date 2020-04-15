1e0d787bb9e9c81093603afcedd0b2c9c609063b
Yura <yura.leshchuk03@gmail.com>
2020-04-15 11:18:43 +0300

    Converter

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-$.getJSON("https://www.cbr-xml-daily.ru/daily_json.js",
 
-    function (data) {
-        $('#usd').html((data.Valute.USD.Value < data.Valute.USD.Previous ? '<i class="fas fa-long-arrow-alt-down"></i>' : '<i class="fas fa-long-arrow-alt-up"></i>') + '<i class="fas fa-dollar-sign"></i>' + data.Valute.USD.Value.toFixed(2) + ' <i class="fas fa-ruble-sign"></i>');
-    });
+    moveValuta=function(){
+        var valuta1=document.getElementById('valuta1'), valuta2=document.getElementById('valuta2'), tmp= valuta1.selectedIndex;
+        valuta1.selectedIndex=valuta2.selectedIndex;
+        valuta2.selectedIndex=tmp;
+        convertNBU();
+    }
+    
+    convertNBU=function(){
+        var valuta1=document.getElementById('valuta1'), valuta2=document.getElementById('valuta2'), input1=document.getElementById('res_valuta1'), input2=document.getElementById('res_valuta2');
+        var res, k1=window.kurs[valuta1.value], k2=window.kurs[valuta2.value];
+        res=(k1*res_valuta1.value)/k2;
+        input2.value=res.toFixed(2);
+    }
+    
+    showConvert=function(cc){
+        if(cc==undefined)cc=['USD','EUR','PLN'];
+        cc.push('UAN');
+        var ajax=new XMLHttpRequest();
+        ajax.open('GET', 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json');
+        ajax.responseType='json';
+        ajax.onload=function(){
+            window.kurs={'UAN':1};
+            for(var i=0; i<this.response.length;i++){
+            kurs[this.response[i].cc]=this.response[i].rate;
+            }
+            var s='';
+            for(var i=0;i<cc.length;i++)
+            s+='<option value="'+cc[i]+'">'+cc[i]+'</option>';
+            document.getElementById('valuta1').innerHTML=s;
+            document.getElementById('valuta2').innerHTML=s;
+            
+        }
+        ajax.send(null);
+    }
+    
+    showConvert(['USD','EUR', 'PLN', 'CAD']);
